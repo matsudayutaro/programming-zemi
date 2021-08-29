4404b4e8f5b83e98cfe363e6b599e1c5d795fcf4
matsudayutaro <matsu7272yuu@gmail.com>
2021-08-29 13:41:31 +0000

    simple tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
     belongs_to :user
-    validates :content, presence: true
-    length: { maximum: 140 }
-end
+    validates :content, presence: true,
+                        length: { maximum: 140 } 
+end 
