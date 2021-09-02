04c237d15e8d2c91b4491a7e9f02a22e955c71aa
matsudayutaro <matsu7272yuu@gmail.com>
2021-09-02 13:49:20 +0000

    静的ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'home'=>"static_pages#home"
+  get 'help'=>"static_pages#help"
+  get 'about'=>"static_pages#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  # 以下のコードを追加してみましょう
  get 'hello-world' => "foo#hello"
-
 end 
