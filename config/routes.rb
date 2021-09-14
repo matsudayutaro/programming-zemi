8594b7552e11b606c2cf5068999ca58c2bb30afc
matsudayutaro <matsu7272yuu@gmail.com>
2021-09-14 10:43:16 +0000

    user controller and model created

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  # 以下のコードを追加してみましょう
  get 'hello-world' => "foo#hello"
+ get  'signup' => "users#new"
 end 
