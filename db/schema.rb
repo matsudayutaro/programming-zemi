8fed9b7fae44944158d82675681acecd5d76e23b
matsudayutaro <matsu7272yuu@gmail.com>
2021-09-18 16:47:04 +0000

    validation and password

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_104201) do
+ActiveRecord::Schema.define(version: 2021_09_18_155741) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
